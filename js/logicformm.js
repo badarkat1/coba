284c3000ec9aa5f6441f99e519ac14e966c56780
badar <katbadar1@gmail.com>
2023-10-12 23:40:35 +0700

    done ongkir

--- js/logicformm.js
+++ js/logicformm.js
@@ -3,6 +3,9 @@ const baseUrl = "https://be-palembang-24-production.up.railway.app/";
 let formCekOngkir = document.getElementById("formpostt");
 const display = document.querySelector("div[name='hasil-query']")
 display.innerHTML = ""
+//let formCekResi = document.getElementById("formpostr");
+//const display2 = document.querySelector("div[name='hasil-query2']")
+//display2.innerHTML = ""
 
 formCekOngkir.addEventListener("submit", (event) =>{
     event.preventDefault();
@@ -23,7 +26,7 @@ formCekOngkir.addEventListener("submit", (event) =>{
         const submittedData = {
             asal: asalValue,
             tujuan: tujuanValue,
-            berat: 15000*quantityValue
+            berat: quantityValue
         };
         
         fetch(`${baseUrl}formpostt`, {
@@ -33,37 +36,87 @@ formCekOngkir.addEventListener("submit", (event) =>{
             },
             body: JSON.stringify(submittedData),
         })
+        .then(response => response.json())
         .then((res) => {
             console.log("test", res)
-            if(res.ok) {
-                alert("Ongkir sudah dapat dilihat");
-            } else {
-                alert("Ongkir belum dapat dilihat");
-            }
-        })
-        .catch((error) => {
-            alert(`Error Massage: ${error.massage}`);
-        })
-        .finally(() => {
+
+            const data = res.data[0];
+            const su_tarif = data[`su_tarif*'${quantityValue}'`]/5;
             const display = document.querySelector("div[name='hasil-query']")
             display.innerHTML = `
             <div class="parentPackageDiv">
                 <div>
-                    <p>Asal Kota : ${submittedData.asalkota}</p>
+                    <p>Asal Kota : ${data.asal}</p>
                 </div>
                 <div>
-                    <p>Tujuan Kota : ${submittedData.tujuankota}</p>
+                    <p>Tujuan Kota : ${data.tujuan}</p>
                 </div>
                 <div>
-                    <p> Harga : ${submittedData.berat}</p>
+                    <p> Harga : ${su_tarif}</p>
                 </div>
                 <div class="button-container">
                     <button type="button" onclick="clearOutput()">Clear</button>
                 </div>
             </div>
             `;
+        })
+        .catch((error) => {
+            alert(`Error Massage: ${error.massage}`);
         });
     document.querySelector("#asal").value = " ";
     document.querySelector("#tujuan").value = " ";
     document.querySelector("#quantity").value = " ";
 });
+
+/*
+formCekResi.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const resiValue = document.querySelector("#resi").value;
+    
+    if (
+        !resiValue
+        ) {
+            alert("Please Fill in all fields correctly before sending ");
+            return;
+        }
+        
+        const submittedData = {
+            resi: resiValue
+        };
+        
+        fetch(`${baseUrl}formpostr`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(submittedData),
+        })
+        .then(response => response.json())
+        .then((res) => {
+            console.log("test", res)
+
+            const data = res.data[0];
+            const display2 = document.querySelector("div[name='hasil-query2']")
+            display2.innerHTML = `
+            <div class="parentPackageDiv">
+                <div>
+                    <p>Resi : ${data.resi}</p>
+                </div>
+                <div>
+                    <p>Penerima : ${data.penerima}</p>
+                </div>
+                <div>
+                    <p>Status : ${data.status}</p>
+                </div>
+                <div class="button-container">
+                    <button type="button" onclick="clearOutput()">Clear</button>
+                </div>
+                </div>
+                `;
+        })
+        .catch((error) => {
+            alert(`Error Massage: ${error.massage}`);
+        })
+    document.querySelector("#resi").value = " ";
+}); */
+        
